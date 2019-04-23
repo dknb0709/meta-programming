03eec8ac266b09b2754ee30de767d696ad518cc1
dknb0709 <ibot07092@gmail.com>
2019-04-24 04:54:50 +0900

    add operator/

--- 3_meta_function/dimensional_analysis.cpp
+++ 3_meta_function/dimensional_analysis.cpp
@@ -1,8 +1,12 @@
+#include <boost/mpl/equal.hpp>
+#include <boost/mpl/placeholders.hpp>
 #include <boost/mpl/transform.hpp>
 #include <boost/mpl/vector_c.hpp>
+#include <boost/static_assert.hpp>
 #include <iostream>
 
 namespace mpl = boost::mpl;
+using namespace mpl::placeholders;
 
 using mass = mpl::vector_c<int, 1, 0, 0, 0, 0, 0, 0>;
 using length = mpl::vector_c<int, 0, 1, 0, 0, 0, 0, 0>;
@@ -20,6 +24,11 @@ template <class T, class Dimensions>
 struct quantity {
   explicit quantity(T x) : m_value(x) {}
   T value() const { return m_value; }
+
+  template <class OtherDimensions>
+  quantity(quantity<T, OtherDimensions> const& rhs) : m_value(rhs.value()) {
+    BOOST_STATIC_ASSERT((mpl::equal<Dimensions, OtherDimensions>::type::value));
+  }
 
  private:
   T m_value;
@@ -47,6 +56,7 @@ struct plus_f {
   };
 };
 
+// operator *
 template <class T, class D1, class D2>
 quantity<T, typename mpl::transform<D1, D2, plus_f>::type> operator*(
     quantity<T, D1> x, quantity<T, D2> y) {
@@ -54,9 +64,31 @@ quantity<T, typename mpl::transform<D1, D2, plus_f>::type> operator*(
   return quantity<T, dim>(x.value() * y.value());
 }
 
+// typedef typename mpl::minus<T1, T2>::type is not
+// necessary anymore, by using inheritance
+// this powerfull simplification is called
+// "metafunction forwarding"
+struct minus_f {
+  template <class T1, class T2>
+  struct apply : mpl::minus<T1, T2> {};
+};
+
+// operator /
+template <class T, class D1, class D2>
+quantity<T, typename mpl::transform<D1, D2, mpl::minus<_1, _2>>::type>
+operator/(quantity<T, D1> x, quantity<T, D2> y) {
+  using dim = typename mpl::transform<D1, D2, mpl::minus<_1, _2>>::type;
+  return quantity<T, dim>(x.value() / y.value());
+}
+
 int main() {
   quantity<float, mass> m(5.0f);
   quantity<float, acceleration> a(9.8f);
 
-  std::cout << (m * a).value() << std::endl;
+  quantity<float, force> f = m * a;
+  std::cout << "force = " << f.value() << std::endl;
+
+  quantity<float, mass> m2 = f / a;
+  std::cout << "mass = " << m2.value() << std::endl;
+  std::cout << "rounding error = " << std::abs((m2 - m).value()) << std::endl;
 }
